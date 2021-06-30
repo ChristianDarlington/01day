ec771e8d5dfcf49c602921c77d7aa48a9b5c4609
bocacode <bocacode@bocacodes-MacBook-Air.local>
2021-06-30 11:13:57 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,46 @@
+// const oreder = {
+// subtotal : 5.00 ,
+// taxtotal : 0.45 ,
+// total :    5.45 ,
+// customerName : Christian ,
+// }
+// order.darlington
+
+// console.log()totalCost 
+
+
+
+
+//const num1 = 200
+//const num2 = 300
+//const num3 = 1000
+//const num4 = 5000
+//const num20 = 2
+
+//function addAmount(sneakers, food) {
+  //const addAmount = sneakers + food
+  //return addAmount
+//}
+
+//console.log(addAmount(num4, num1))
+
+
+
+// const num1 = 8
+// function superman(number){
+//   const superman = number**2
+//   return superman
+// }
+// console.log(superman(num1))
+
+
+
+ages =[20,40,60,80,100]
+
+function agecalc (ages){
+for(let i=0; i < ages.length; i++){
+  console.log(ages[i]**2)
+ }
+}
+agecalc(ages)
+
